95b4bf01e14fa4bf70061408ea37672da420e25e
Jaime Azcarate <75700354+jaimeandres00@users.noreply.github.com>
2021-11-24 16:14:09 -0500

    Despliegue del backend en heroku

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 require('dotenv').config({
    path: './config/index.env'
