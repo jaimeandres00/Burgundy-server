e9d343a79dc0bca835a634a18bcf837f220393f5
Jaime Azcarate <75700354+jaimeandres00@users.noreply.github.com>
2021-12-07 00:07:02 -0500

    Backend finalizado

--- middleware/checkServiceBelongsToUser.js
+++ middleware/checkServiceBelongsToUser.js
@@ -5,19 +5,21 @@ module.exports = async function (req, res, next) {
     const { serviceId } = req.params;
 
     try {
-        let service = await Service.findById(serviceId).select('user');
+        const service = await Service.findById(serviceId).select('user');
         const user = await User.findOne({ _id: req.user.id }).select('role');
-   
-        if(user.role == 0 || (user.role == 1 && service.user !== req.user.id )){
+
+        if(user.role == 2){
+            next();
+        } else if(req.user.id == service.user) {
+            next();
+        } else {
             return res.status(401).json({
                 error: 'Autorización denegada'
-            });            
-        }
+            });              
+        } 
 
     } catch (error) {
         console.log(error);
         res.send('Error del servidor');
     }
-
-    next();    
 };
